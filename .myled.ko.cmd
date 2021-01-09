66148e16990137876bef029d36037b77daa75a91
rensho215 <72724172+rensho215@users.noreply.github.com>
2021-01-09 18:11:29 +0900

    Delete .myled.ko.cmd

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/DeviceDriver/myled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/DeviceDriver/myled.ko /home/ubuntu/DeviceDriver/myled.o /home/ubuntu/DeviceDriver/myled.mod.o;  true
